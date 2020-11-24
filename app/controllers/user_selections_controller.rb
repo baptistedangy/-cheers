fd3e542081963f91fc7f0f920290f2b5bc356e0f
baptistedangy <baptiste1.dangy@gmail.com>
2020-11-24 11:41:18 +0100

    setup

---
+++ app/controllers/user_selections_controller.rb
@@ -0,0 +1,11 @@
+class UserSelectionsController < ApplicationController
+  def index
+    @user_selections = User_selection.all
+  end
+
+  def destroy
+    @user_selection = User_selection.find(params[:id])
+    @user_selection.destroy
+    redirect_to user_selections_path
+  end
+end
