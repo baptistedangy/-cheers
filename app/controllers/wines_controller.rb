d2cbaaa4edabafcede10d35caee74eca3a34b914
baptistedangy <baptiste1.dangy@gmail.com>
2020-11-25 16:52:27 +0100

    homepage

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,16 +1,16 @@
 class WinesController < ApplicationController
   def index
-    search = "Blanquette de Veau"
-    # to replace with search result if correct
-    @meal = Meal.all.find do |meal|
-      meal.name == search
-    end
-    @wines = Wine.all
-    @suggestion = Suggestion.all.find do |suggestion|
-      (suggestion.main_category == @meal.main_category) && (suggestion.cooking_method == @meal.cooking_method)
-    end
-    @wine_selection = @wines.select do |wine|
-      (wine.wine_region == @suggestion.wine_region) && (wine.wine_type == @suggestion.wine_type)
+    if params[:query].present?
+      @meal = Meal.find_by(name: params[:query])
+      @wines = Wine.all
+      @suggestion = Suggestion.all.find do |suggestion|
+        (suggestion.main_category == @meal.main_category) && (suggestion.cooking_method == @meal.cooking_method)
+      end
+      @wine_selection = @wines.select do |wine|
+        (wine.wine_region == @suggestion.wine_region) && (wine.wine_type == @suggestion.wine_type)
+      end
+    else
+      @wine_selection = Wine.all
     end
   end
 
