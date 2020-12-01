9476a179b8b0ffcaadd2d49b2c1a1720621f6767
baptistedangy <baptiste1.dangy@gmail.com>
2020-12-01 20:14:36 +0100

    changes

--- app/javascript/plugins/init_star_rating.js
+++ app/javascript/plugins/init_star_rating.js
@@ -10,3 +10,9 @@ const initStarRating = () => {
 };
 
 export { initStarRating };
+
+
+
+// onSelect: (value, text, event) => {
+//       const form = $("form.review_form"); // We are selecting the form on the page with its class
+//       form.submit(); // We submit the form with javascript
